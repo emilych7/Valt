69fb3c7c95357a4dce1bf3ce4736b0c6286518c3
Emily Hernandez <emilychernandez17@gmail.com>
2025-06-06 13:21:42 -0700

    Initial Commit

---
+++ Valt/ValtApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValtApp.swift
+//  Valt
+//
+//  Created by Emily Chrisann Hernandez on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ValtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ValtTests/ValtTests.swift
@@ -0,0 +1,17 @@
+//
+//  ValtTests.swift
+//  ValtTests
+//
+//  Created by Emily Chrisann Hernandez on 6/6/25.
+//
+
+import Testing
+@testable import Valt
+
+struct ValtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
