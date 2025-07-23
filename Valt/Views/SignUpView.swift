f7bdb12d98987f5a45bb6f1db9fc3c56968f6827
Emily Hernandez <emilychernandez17@gmail.com>
2025-07-22 18:34:11 -0700

    adding .gitignore, UI updates to Login/SignUp/Profile Views

--- Valt/Views/SignUpView.swift
+++ Valt/Views/SignUpView.swift
@@ -1,15 +1,14 @@
 import SwiftUI
 
-struct LoginView: View {
+struct SignUpView: View {
     @EnvironmentObject var authViewModel: AuthViewModel
     @State private var email = ""
     @State private var password = ""
     
     var body: some View {
         VStack {
-            HStack{
-                
-                Text("Welcome back")
+            HStack {
+                Text("Create an account")
                     .font(.custom("OpenSans-SemiBold", size: 30))
                     .foregroundColor(Color("TextColor"))
                 
@@ -46,20 +45,37 @@ struct LoginView: View {
             .padding(.top, 20)
             .padding(.bottom, 5)
             
-            VStack (spacing: 5) {
+            VStack (spacing: 5){
                 HStack {
                     Text("Password")
                         .font(.custom("OpenSans-Regular", size: 17))
                         .foregroundColor(Color("TextColor"))
                     
                     Spacer ()
+                }
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(height: 50)
+                        .foregroundColor(Color("TextFieldBackground"))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color("TextFieldBorder"), lineWidth: 1) // border
+                            )
+                    SecureField("              ", text: $password)
+                        .textFieldStyle(.plain)
+                        .padding(.horizontal)
+                }
+            }
+            .padding(.top, 5)
+            
+            VStack (spacing: 5){
+                HStack {
+                    Text("Re-Enter Password")
+                        .font(.custom("OpenSans-Regular", size: 17))
+                        .foregroundColor(Color("TextColor"))
                     
-                    Button("Forgot Password?") {
-                        Task {
-                            await authViewModel.resetPassword(email: email)
-                        }
-                    }
-                    .font(.custom("OpenSans-Regular", size: 15))
+                    Spacer ()
                 }
                 
                 ZStack {
@@ -78,7 +94,7 @@ struct LoginView: View {
             .padding(.top, 5)
             
             VStack {
-                Button("Log In") {
+                Button("Sign Up") {
                     Task {
                         await authViewModel.signIn(email: email, password: password)
                     }
@@ -92,7 +108,7 @@ struct LoginView: View {
             .padding(.top, 15)
             
             ZStack {
-                Text("or login using")
+                Text("or sign up using")
                     .foregroundColor(Color("TextColor"))
                     .font(.custom("OpenSans-Regular", size: 16))
             }
@@ -130,17 +146,15 @@ struct LoginView: View {
             
             ZStack {
                 HStack (spacing: 3) {
-                    Text("No account?")
+                    Text("Already have an account?")
                         .foregroundColor(Color("TextColor"))
                         .font(.custom("OpenSans-Regular", size: 17))
-                    Button("Let's make one.") {
+                    Button("Log in.") {
                         // Navigate to SignUpView later
                     }
                 }
             }
             .padding(.vertical, 10)
-            
-            Spacer()
         }
         .padding(.horizontal, 40)
         .padding(.bottom, 50)
